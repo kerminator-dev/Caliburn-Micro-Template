b0f041df4d39f3347de41de9d1a3a63c1f1b3a3c
Kerminator-dev <pussy_hunter@list.ru>
2023-01-09 16:36:59 +0300

    Проект создан

---
+++ CMTemplate/WPFUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,91 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFUI.Models;
+
+namespace WPFUI.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+        private string _firstName = "";
+        public string FirstName 
+        {
+            get => _firstName;
+            set
+            {
+                _firstName = value;
+
+                base.NotifyOfPropertyChange(() => FirstName);
+                base.NotifyOfPropertyChange(() => FullName);
+            }
+        }
+
+        private string _lastName = "";
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                _lastName = value;
+
+                base.NotifyOfPropertyChange(() => LastName);
+                base.NotifyOfPropertyChange(() => FullName);
+            }
+        }
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        private BindableCollection<Person> _people;
+        public BindableCollection<Person> People
+        {
+            get => _people;
+            set => _people = value;
+        }
+
+        private Person? _selectedPerson;
+
+        public Person? SelectedPerson
+        {
+            get => _selectedPerson;
+            set
+            {
+                _selectedPerson = value;
+
+                NotifyOfPropertyChange(() => SelectedPerson);
+            }
+        }
+
+        public ShellViewModel()
+        {
+            People = new BindableCollection<Person>()
+            {
+                new Person() { FirstName = "John", LastName = "Delman" },
+                new Person() { FirstName = "Lucy", LastName = "Jenn" }
+            };
+        }
+
+        public void ClearText()
+        {
+            FirstName = "";
+            LastName = "";
+        }
+
+        public bool CanClearText(string firstName, string lastName)
+        {
+            return !string.IsNullOrEmpty(firstName) || !string.IsNullOrEmpty(lastName);
+        }
+
+        public async void LoadPageOne()
+        {
+            await base.ActivateItemAsync(new FirstChildViewModel());
+        }
+
+        public async void LoadPageTwo()
+        {
+            await base.ActivateItemAsync(new SecondChildViewModel());
+        }
+    }
+}
